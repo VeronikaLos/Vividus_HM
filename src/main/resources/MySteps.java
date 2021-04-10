aa7e8ebbbeb730bca9303d6431ca19b4fae1ba99
Veranika Los <Veranika_Los@epam.com>
2021-04-10 17:46:12 +0300

    Homework

---
+++ src/main/resources/MySteps.java
@@ -0,0 +1,28 @@
+import org.jbehave.core.annotations.*;
+public class MySteps{
+	@When("I open the product")
+	@Pending
+	public void whenIOpenTheProduct(){
+		 //TODO 
+	}
+	@When("I open the cart")
+	@Pending
+	public void whenIOpenTheCart(){
+		 //TODO 
+	}
+	@When("I search the product")
+	@Pending
+	public void whenISearchTheProduct(){
+		 //TODO 
+	}
+	@When("I add the product to cart")
+	@Pending
+	public void whenIAddTheProductToCart(){
+		 //TODO 
+	}
+	@When("I fill in the registration form")
+	@Pending
+	public void whenIFillInTheRegistrationForm(){
+		 //TODO 
+	}
+}
